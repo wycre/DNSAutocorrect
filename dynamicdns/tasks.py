e1d51cc065ce18fc469ea565e452868f07502110
wycre <djones@wycre.net>
2024-12-06 15:50:22 -0700

    implement dns_engine

--- dynamicdns/tasks.py
+++ dynamicdns/tasks.py
@@ -3,6 +3,11 @@ Celery tasks for dynamicdns.
 """
 
 from celery import shared_task
+from requests import get
+import dynamicdns.dns_providers as dns_providers
+
+from dynamicdns.models import MonitoredRecord, DNSService
+
 
 @shared_task
 def run_dns_engine():
@@ -10,4 +15,48 @@ def run_dns_engine():
     Performs the checks on all DNS records based on timing.
     :return:
     """
-    pass
+
+    # Pull DB Data
+    records = MonitoredRecord.objects.all()
+    services = DNSService.objects.all()
+
+    # Extract record names
+    record_names={}
+    for record in records:
+        record_names[record.name] = (record.dynamic_source_of_truth, record.source_of_truth)
+
+    # Main loop
+    for service in services:
+        (get_records, update_record) = dns_providers.upa_resolver(service.provider)
+
+        service_records = get_records(service.service_data)
+
+        for record in service_records:
+
+            # If this service record is a monitored record
+            if record[0] in record_names:
+
+                # If source of truth is dynamic
+                if record_names[record[0]][0]:
+                    truth = get("https://api.ipify.org").text
+
+                    if record[2] != truth:
+                        print(f"{record[0]} MISMATCH: {record[2]} != {truth}")
+                        new_record = record
+                        new_record[2] = truth
+
+                        if not update_record(service.service_data, record, new_record):
+                            print(f"{record[0]} UPDATE FAILED")
+
+
+                # If source of truth is static
+                if record_names[record[0]][1]:
+                    truth = record_names[record[0]][1]
+                    if record[2] != truth:
+                        print(f"{record[0]} MISMATCH: {record[2]} != {truth}")
+                        new_record = record
+                        new_record[2] = truth
+
+                        if not update_record(service.service_data, record, new_record):
+                            print(f"{record[0]} UPDATE FAILED")
+
