27e9e62b939d45b5a5aa148303928f38c4961a80
wycre <djones@wycre.net>
2024-12-10 16:00:11 -0700

    Namecheap support abandoned

--- moduleNamecheap/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from dynamicdns.forms import DNSServiceForm
-
-class NamecheapServiceForm(DNSServiceForm):
-    zone_id = forms.CharField()
-    zone_name = forms.CharField()
